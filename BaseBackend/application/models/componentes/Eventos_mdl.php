6667694bd1eea234dee120bcedde07fb9ae2126b
Juan Pablo Medina <juan@ayudatelta.com>
2019-10-06 19:14:21 -0500

    Agrega ver imagenes a evento ws

--- BaseBackend/application/models/componentes/Eventos_mdl.php
+++ BaseBackend/application/models/componentes/Eventos_mdl.php
@@ -24,29 +24,16 @@ class Eventos_mdl extends CI_Model{
         $this->db->where('e.id',$evento_id);
         return $this->db->get()->result();
     }
-    /*public function archivos_todos($sede,$tipo)
-    {   
-        switch($tipo){
-            case "imagenes":
 
-                break;
-            case "banners":
-                break;
-            case "videos":
-                break;
-        }
+    public function imagenes($evento_id)
+    {   
         $this->db->select("a.ruta");
-        $this->db->from('lugar as l');
-        $this->db->join('galeria_lugar as g_l','l.id = g_l.lugar_fk','inner');
-        $this->db->join('archivo as a','g_l.archivo_fk = a.id','inner');
-        $this->db->join('tipo_archivo_cat as t_a','a.tipo_fk = t_a.id','inner');
-        $this->db->where(array(
-            't_a.tipo'=>$tipo,
-            'l.nombre'=>$sede
-        ));
+        $this->db->from('archivo AS a');
+        $this->db->join('evento_archivo as ae','ae.archivo_fk = a.id','inner');
+        $this->db->where('ae.evento_fk',$evento_id);
         return $this->db->get()->result();
     
-    }*/
+    }
     
     public function __destruct()
     {
